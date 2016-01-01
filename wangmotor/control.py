bd569f8f8328784fe1af843c2fb2360e5b32725e
Jeffrey Wang <jeffw16@users.noreply.github.com>
2016-01-01 12:12:35 -0600

    Adding assigning of custom pins

--- wangmotor/control.py
+++ wangmotor/control.py
@@ -9,13 +9,17 @@ class control:
 	pinl = 29
 	pinr = 40
 	
-	def __init__ ( gpio ):
+	def __init__ ( gpios ):
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setwarnings(False)
 		# Set up GPIO pins
-		# goes through array gpio, sets up each GPIO to be outputs
-		for x in xrange(0, len(gpio)):
-			GPIO.setup( gpio[x], GPIO.OUT)
+		# goes through array gpios, sets up each GPIO to be outputs
+		for x in xrange(0, len(gpios)):
+			GPIO.setup( gpios[x], GPIO.OUT)
+		pinf = gpios[0]
+		pinb = gpios[1]
+		pinl = gpios[2]
+		pinr = gpios[3]
 	
 	def __del__ ():
 		GPIO.cleanup()
